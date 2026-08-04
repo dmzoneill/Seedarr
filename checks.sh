c211fced3ddc2c245b529adf4286a82abae362e5
David O Neill <dmz.oneill@gmail.com>
2026-08-04 15:35:11 +0100

    fix(ci): fix shellcheck and shfmt formatting in checks.sh and disable bash linters

--- checks.sh
+++ checks.sh
@@ -3,12 +3,14 @@ set -e
 
 # Disable linters not supported in dispatch.yaml inputs or incompatible with repo
 if [ -n "$GITHUB_ENV" ]; then
-  echo "VALIDATE_TYPESCRIPT_PRETTIER=false" >> "$GITHUB_ENV"
-  echo "VALIDATE_MARKDOWN_PRETTIER=false" >> "$GITHUB_ENV"
-  echo "VALIDATE_SPELL_CODESPELL=false" >> "$GITHUB_ENV"
-  echo "VALIDATE_TYPESCRIPT_ES=false" >> "$GITHUB_ENV"
-  echo "VALIDATE_MARKDOWN=false" >> "$GITHUB_ENV"
-  echo "VALIDATE_NATURAL_LANGUAGE=false" >> "$GITHUB_ENV"
+	{
+		echo "VALIDATE_TYPESCRIPT_PRETTIER=false"
+		echo "VALIDATE_MARKDOWN_PRETTIER=false"
+		echo "VALIDATE_SPELL_CODESPELL=false"
+		echo "VALIDATE_TYPESCRIPT_ES=false"
+		echo "VALIDATE_MARKDOWN=false"
+		echo "VALIDATE_NATURAL_LANGUAGE=false"
+	} >>"$GITHUB_ENV"
 fi
 
 echo 0
