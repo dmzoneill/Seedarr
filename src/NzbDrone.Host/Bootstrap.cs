c358f2ffdafc3952ae2e6da20b8c183c20b2929f
David O Neill <dmz.oneill@gmail.com>
2026-08-13 23:12:20 +0100

    Fix build: simplify Swagger config, remove unused using in Bootstrap

--- src/NzbDrone.Host/Bootstrap.cs
+++ src/NzbDrone.Host/Bootstrap.cs
@@ -4,7 +4,6 @@ using DryIoc.Microsoft.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using NLog;
 using NzbDrone.Common.Composition;
 using NzbDrone.Common.EnvironmentInfo;
