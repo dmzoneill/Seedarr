d6467cdfcfe6289a0a04bc03eae6261b53b212d2
David O Neill <dmz.oneill@gmail.com>
2026-08-14 12:00:12 +0100

    Add SVG icons and integrate into frontend navigation
    - SeedarrLogo inline SVG component with seed/sprout motif
    - 9 nav icon components (Dashboard, Torrent, Settings, System, etc.)
    - Favicon with theme-color and description meta tags
    - CopyWebpackPlugin for favicon output
    - Icons integrated into header logo and nav links with hover effects

--- src/Seedarr.Frontend/webpack.config.js
+++ src/Seedarr.Frontend/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isDev = argv.mode === 'development';
@@ -60,6 +61,11 @@ module.exports = (env, argv) => {
     plugins: [
       new HtmlWebpackPlugin({
         template: './src/index.html',
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          { from: './src/assets/favicon.svg', to: 'favicon.svg' },
+        ],
       }),
       ...(!isDev
         ? [
