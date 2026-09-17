627188516984a479dadc6b2fc6d91238751dd0a0
David O Neill <dmz.oneill@gmail.com>
2026-09-17 06:14:59 +0100

    security(metadata): enforce 10 MiB upper bound limit and SHA-1 cryptographic validation on reassembled metadata (Closes #560)

--- src/NzbDrone.Core.Test/Peers/Extensions/MetadataExchangeTest.cs
+++ src/NzbDrone.Core.Test/Peers/Extensions/MetadataExchangeTest.cs
@@ -1,4 +1,6 @@
+using System;
 using System.IO;
+using System.Security.Cryptography;
 using BencodeNET.Objects;
 using BencodeNET.Parsing;
 using NUnit.Framework;
@@ -173,6 +175,208 @@ public class MetadataExchangeTest
         Assert.That(parsed.TotalSize, Is.EqualTo(4096));
         Assert.That(parsed.Data, Is.Not.Null);
         Assert.That(parsed.Data, Is.EqualTo(rawData));
+    }
+
+    [Test]
+    public void ParseMetadataMessage_should_reject_when_total_size_exceeds_max_metadata_size()
+    {
+        var dict = new BDictionary
+        {
+            ["msg_type"] = new BNumber(1),
+            ["piece"] = new BNumber(0),
+            ["total_size"] = new BNumber(MetadataExchange.MaxMetadataSize + 1)
+        };
+        var encoded = dict.EncodeAsBytes();
+
+        var result = _exchange.ParseMetadataMessage(encoded);
+
+        Assert.That(result.MessageType, Is.EqualTo(2));
+        Assert.That(result.Data, Is.Null);
+    }
+
+    [Test]
+    public void ParseMetadataMessage_should_reject_when_total_size_is_negative()
+    {
+        var dict = new BDictionary
+        {
+            ["msg_type"] = new BNumber(1),
+            ["piece"] = new BNumber(0),
+            ["total_size"] = new BNumber(-1)
+        };
+        var encoded = dict.EncodeAsBytes();
+
+        var result = _exchange.ParseMetadataMessage(encoded);
+
+        Assert.That(result.MessageType, Is.EqualTo(2));
+        Assert.That(result.Data, Is.Null);
+    }
+
+    [Test]
+    public void ParseMetadataMessage_should_reject_when_response_has_zero_total_size()
+    {
+        var dict = new BDictionary
+        {
+            ["msg_type"] = new BNumber(1),
+            ["piece"] = new BNumber(0),
+            ["total_size"] = new BNumber(0)
+        };
+        var encoded = dict.EncodeAsBytes();
+
+        var result = _exchange.ParseMetadataMessage(encoded);
+
+        Assert.That(result.MessageType, Is.EqualTo(2));
+        Assert.That(result.Data, Is.Null);
+    }
+
+    [Test]
+    public void ParseMetadataMessage_should_reject_when_chunk_size_exceeds_max_block_size()
+    {
+        var oversizedChunk = new byte[MetadataExchange.MetadataBlockSize + 1];
+        var encoded = _exchange.BuildMetadataResponse(0, 32768, oversizedChunk);
+
+        var result = _exchange.ParseMetadataMessage(encoded);
+
+        Assert.That(result.MessageType, Is.EqualTo(2));
+        Assert.That(result.Data, Is.Null);
+    }
+
+    [Test]
+    public void ParseMetadataMessage_should_reject_when_final_chunk_size_exceeds_expected_size()
+    {
+        var invalidFinalChunk = new byte[4000];
+        var encoded = _exchange.BuildMetadataResponse(1, 20000, invalidFinalChunk);
+
+        var result = _exchange.ParseMetadataMessage(encoded);
+
+        Assert.That(result.MessageType, Is.EqualTo(2));
+        Assert.That(result.Data, Is.Null);
+    }
+
+    [Test]
+    public void ParseMetadataMessage_should_accept_valid_final_chunk_size()
+    {
+        var validFinalChunk = new byte[3616];
+        var encoded = _exchange.BuildMetadataResponse(1, 20000, validFinalChunk);
+
+        var result = _exchange.ParseMetadataMessage(encoded);
+
+        Assert.That(result.MessageType, Is.EqualTo(1));
+        Assert.That(result.Piece, Is.EqualTo(1));
+        Assert.That(result.TotalSize, Is.EqualTo(20000));
+        Assert.That(result.Data, Is.Not.Null);
+        Assert.That(result.Data.Length, Is.EqualTo(3616));
+    }
+
+    [Test]
+    public void BuildMetadataReject_should_contain_msg_type_two_and_piece()
+    {
+        var result = _exchange.BuildMetadataReject(3);
+        var dict = ParseBencode(result);
+
+        Assert.That((int)((BNumber)dict["msg_type"]).Value, Is.EqualTo(2));
+        Assert.That((int)((BNumber)dict["piece"]).Value, Is.EqualTo(3));
+    }
+
+    [Test]
+    public void ValidateMetadata_should_accept_when_hash_matches()
+    {
+        var metadataBytes = new byte[1024];
+        new Random(42).NextBytes(metadataBytes);
+        var expectedHash = Convert.ToHexString(SHA1.HashData(metadataBytes));
+
+        var isValidUpper = _exchange.ValidateMetadata(metadataBytes, expectedHash.ToUpperInvariant());
+        var isValidLower = _exchange.ValidateMetadata(metadataBytes, expectedHash.ToLowerInvariant());
+
+        Assert.That(isValidUpper, Is.True);
+        Assert.That(isValidLower, Is.True);
+    }
+
+    [Test]
+    public void ValidateMetadata_should_reject_when_hash_mismatches()
+    {
+        var metadataBytes = new byte[1024];
+        new Random(42).NextBytes(metadataBytes);
+        const string mismatchedHash = "0123456789ABCDEF0123456789ABCDEF01234567";
+
+        var isValid = _exchange.ValidateMetadata(metadataBytes, mismatchedHash);
+
+        Assert.That(isValid, Is.False);
+    }
+
+    [Test]
+    public void ValidateMetadata_should_reject_when_tampered()
+    {
+        var metadataBytes = new byte[1024];
+        new Random(42).NextBytes(metadataBytes);
+        var expectedHash = Convert.ToHexString(SHA1.HashData(metadataBytes));
+
+        metadataBytes[0] ^= 0xFF;
+
+        var isValid = _exchange.ValidateMetadata(metadataBytes, expectedHash);
+
+        Assert.That(isValid, Is.False);
+    }
+
+    [Test]
+    public void ReassembleMetadata_should_reassemble_valid_pieces_matching_infohash()
+    {
+        const int totalSize = 20000;
+        var piece0 = new byte[MetadataExchange.MetadataBlockSize];
+        var piece1 = new byte[totalSize - MetadataExchange.MetadataBlockSize];
+        new Random(123).NextBytes(piece0);
+        new Random(456).NextBytes(piece1);
+
+        var expectedMetadata = new byte[totalSize];
+        Array.Copy(piece0, 0, expectedMetadata, 0, piece0.Length);
+        Array.Copy(piece1, 0, expectedMetadata, piece0.Length, piece1.Length);
+
+        var expectedHash = Convert.ToHexString(SHA1.HashData(expectedMetadata));
+        var pieces = new[] { piece0, piece1 };
+
+        var reassembled = _exchange.ReassembleMetadata(pieces, totalSize, expectedHash);
+
+        Assert.That(reassembled, Is.Not.Null);
+        Assert.That(reassembled.Length, Is.EqualTo(totalSize));
+        Assert.That(reassembled, Is.EqualTo(expectedMetadata));
+    }
+
+    [Test]
+    public void ReassembleMetadata_should_discard_and_return_null_when_hash_mismatches()
+    {
+        const int totalSize = 20000;
+        var piece0 = new byte[MetadataExchange.MetadataBlockSize];
+        var piece1 = new byte[totalSize - MetadataExchange.MetadataBlockSize];
+        new Random(123).NextBytes(piece0);
+        new Random(456).NextBytes(piece1);
+
+        const string wrongHash = "0000000000000000000000000000000000000000";
+        var pieces = new[] { piece0, piece1 };
+
+        var reassembled = _exchange.ReassembleMetadata(pieces, totalSize, wrongHash);
+
+        Assert.That(reassembled, Is.Null);
+    }
+
+    [Test]
+    public void ReassembleMetadata_should_discard_and_return_null_when_total_size_invalid()
+    {
+        var pieces = new[] { new byte[100] };
+
+        Assert.That(_exchange.ReassembleMetadata(pieces, -1, "hash"), Is.Null);
+        Assert.That(_exchange.ReassembleMetadata(pieces, MetadataExchange.MaxMetadataSize + 1, "hash"), Is.Null);
+    }
+
+    [Test]
+    public void ReassembleMetadata_should_discard_and_return_null_when_chunk_size_invalid()
+    {
+        const int totalSize = 20000;
+        var piece0 = new byte[10000];
+        var piece1 = new byte[totalSize - 10000];
+        var pieces = new[] { piece0, piece1 };
+
+        var reassembled = _exchange.ReassembleMetadata(pieces, totalSize, "hash");
+
+        Assert.That(reassembled, Is.Null);
     }
 
     private static BDictionary ParseBencode(byte[] data)
--- src/NzbDrone.Core/Peers/Extensions/MetadataExchange.cs
+++ src/NzbDrone.Core/Peers/Extensions/MetadataExchange.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Security.Cryptography;
 using BencodeNET.Objects;
 using BencodeNET.Parsing;
 using NLog;
@@ -8,9 +10,18 @@ namespace NzbDrone.Core.Peers.Extensions;
 
 public interface IMetadataExchange
 {
+    const int MaxMetadataSize = 10 * 1024 * 1024; // 10 MiB
+    const int MetadataBlockSize = 16384; // 16 KiB
+
     byte[] BuildMetadataRequest(int piece);
     byte[] BuildMetadataResponse(int piece, int totalSize, byte[] data);
+    byte[] BuildMetadataReject(int piece);
     MetadataMessage ParseMetadataMessage(byte[] data);
+    bool ValidateMetadata(byte[] assembledRawBytes, string expectedInfoHash, string torrentName = null);
+    bool VerifyMetadataHash(byte[] assembledRawBytes, string expectedInfoHash, string torrentName = null);
+    byte[] ReassembleMetadata(byte[][] pieces, int totalSize, string expectedInfoHash, string torrentName = null);
+    byte[] ReassembleMetadata(IEnumerable<MetadataMessage> messages, int totalSize, string expectedInfoHash, string torrentName = null);
+    bool TryReassembleMetadata(byte[][] pieces, int totalSize, string expectedInfoHash, out byte[] assembledMetadata, string torrentName = null);
 }
 
 public class MetadataMessage
@@ -23,6 +34,9 @@ public class MetadataMessage
 
 public class MetadataExchange : IMetadataExchange
 {
+    public const int MaxMetadataSize = 10 * 1024 * 1024; // 10 MiB
+    public const int MetadataBlockSize = 16384; // 16 KiB
+
     private readonly Logger _logger;
 
     public MetadataExchange()
@@ -57,6 +71,17 @@ public class MetadataExchange : IMetadataExchange
         return result;
     }
 
+    public byte[] BuildMetadataReject(int piece)
+    {
+        var dict = new BDictionary
+        {
+            ["msg_type"] = new BNumber(2),
+            ["piece"] = new BNumber(piece)
+        };
+
+        return dict.EncodeAsBytes();
+    }
+
     public MetadataMessage ParseMetadataMessage(byte[] data)
     {
         if (data == null || data.Length == 0)
@@ -70,14 +95,6 @@ public class MetadataExchange : IMetadataExchange
             using var stream = new MemoryStream(data);
             var dict = parser.Parse<BDictionary>(stream);
 
-            byte[] pieceData = null;
-            if (stream.Position < stream.Length)
-            {
-                var remaining = (int)(stream.Length - stream.Position);
-                pieceData = new byte[remaining];
-                stream.ReadExactly(pieceData, 0, remaining);
-            }
-
             var messageType = dict.TryGetValue("msg_type", out var msgTypeObj) && msgTypeObj is BNumber msgTypeNum
                 ? (int)msgTypeNum.Value
                 : 0;
@@ -86,9 +103,65 @@ public class MetadataExchange : IMetadataExchange
                 ? (int)pieceNum.Value
                 : 0;
 
-            var totalSize = dict.TryGetValue("total_size", out var totalSizeObj) && totalSizeObj is BNumber totalSizeNum
-                ? (int)totalSizeNum.Value
-                : 0;
+            if (piece < 0)
+            {
+                _logger.Warn("Peer reported invalid metadata piece index: {0}", piece);
+                return new MetadataMessage
+                {
+                    MessageType = 2,
+                    Piece = piece
+                };
+            }
+
+            var hasTotalSize = dict.TryGetValue("total_size", out var totalSizeObj) && totalSizeObj is BNumber totalSizeNum;
+            var totalSize = hasTotalSize ? (int)((BNumber)totalSizeObj).Value : 0;
+
+            if ((hasTotalSize && (totalSize <= 0 || totalSize > MaxMetadataSize)) ||
+                (messageType == 1 && (totalSize <= 0 || totalSize > MaxMetadataSize)))
+            {
+                _logger.Warn("Peer reported invalid metadata total_size: {0} bytes (max allowed: {1})", totalSize, MaxMetadataSize);
+                return new MetadataMessage
+                {
+                    MessageType = 2,
+                    Piece = piece
+                };
+            }
+
+            byte[] pieceData = null;
+            if (stream.Position < stream.Length)
+            {
+                var remaining = stream.Length - stream.Position;
+                if (remaining > MetadataBlockSize)
+                {
+                    _logger.Warn("Incoming metadata chunk size {0} exceeds max block size {1}", remaining, MetadataBlockSize);
+                    return new MetadataMessage
+                    {
+                        MessageType = 2,
+                        Piece = piece
+                    };
+                }
+
+                if (messageType == 1 && totalSize > 0)
+                {
+                    var totalPieces = (int)Math.Ceiling((double)totalSize / MetadataBlockSize);
+                    if (piece == totalPieces - 1)
+                    {
+                        var expectedSize = totalSize - (piece * MetadataBlockSize);
+                        if (expectedSize > 0 && remaining > expectedSize)
+                        {
+                            _logger.Warn("Incoming metadata chunk size {0} exceeds expected size {1} for final piece {2}", remaining, expectedSize, piece);
+                            return new MetadataMessage
+                            {
+                                MessageType = 2,
+                                Piece = piece
+                            };
+                        }
+                    }
+                }
+
+                pieceData = new byte[remaining];
+                stream.ReadExactly(pieceData, 0, (int)remaining);
+            }
 
             return new MetadataMessage
             {
@@ -103,5 +176,142 @@ public class MetadataExchange : IMetadataExchange
             _logger.Debug(ex, "Failed to parse metadata message");
             return new MetadataMessage();
         }
+    }
+
+    public bool ValidateMetadata(byte[] assembledRawBytes, string expectedInfoHash, string torrentName = null)
+    {
+        if (assembledRawBytes == null || assembledRawBytes.Length == 0)
+        {
+            _logger.Warn("Cannot validate metadata: raw bytes are null or empty");
+            return false;
+        }
+
+        if (string.IsNullOrWhiteSpace(expectedInfoHash))
+        {
+            _logger.Warn("Cannot validate metadata: expected infohash is missing");
+            return false;
+        }
+
+        if (assembledRawBytes.Length > MaxMetadataSize)
+        {
+            _logger.Warn("Assembled metadata size {0} exceeds max allowed {1}", assembledRawBytes.Length, MaxMetadataSize);
+            return false;
+        }
+
+        var hash = SHA1.HashData(assembledRawBytes);
+        var calculatedHash = Convert.ToHexString(hash);
+
+        if (!calculatedHash.Equals(expectedInfoHash, StringComparison.OrdinalIgnoreCase))
+        {
+            _logger.Error(
+                "Metadata hash mismatch for {0}: expected {1}, got {2}. Discarding poisoned metadata.",
+                torrentName ?? "torrent",
+                expectedInfoHash,
+                calculatedHash);
+            return false;
+        }
+
+        return true;
+    }
+
+    public bool ValidateMetadata(byte[] assembledRawBytes, byte[] expectedInfoHashBytes, string torrentName = null)
+    {
+        if (expectedInfoHashBytes == null)
+        {
+            return false;
+        }
+
+        return ValidateMetadata(assembledRawBytes, Convert.ToHexString(expectedInfoHashBytes), torrentName);
+    }
+
+    public bool VerifyMetadataHash(byte[] assembledRawBytes, string expectedInfoHash, string torrentName = null)
+    {
+        return ValidateMetadata(assembledRawBytes, expectedInfoHash, torrentName);
+    }
+
+    public byte[] ReassembleMetadata(byte[][] pieces, int totalSize, string expectedInfoHash, string torrentName = null)
+    {
+        if (totalSize <= 0 || totalSize > MaxMetadataSize)
+        {
+            _logger.Warn("Cannot reassemble metadata: total_size {0} is invalid (max allowed: {1})", totalSize, MaxMetadataSize);
+            return null;
+        }
+
+        if (pieces == null)
+        {
+            _logger.Warn("Cannot reassemble metadata: pieces array is null");
+            return null;
+        }
+
+        var totalPieces = (int)Math.Ceiling((double)totalSize / MetadataBlockSize);
+        if (pieces.Length < totalPieces)
+        {
+            _logger.Warn("Cannot reassemble metadata: expected {0} pieces but only have {1}", totalPieces, pieces.Length);
+            return null;
+        }
+
+        var assembledRawBytes = new byte[totalSize];
+        var offset = 0;
+
+        for (var i = 0; i < totalPieces; i++)
+        {
+            var piece = pieces[i];
+            if (piece == null)
+            {
+                _logger.Warn("Cannot reassemble metadata: piece {0} is missing", i);
+                return null;
+            }
+
+            var expectedChunkSize = (i == totalPieces - 1)
+                ? totalSize - (i * MetadataBlockSize)
+                : MetadataBlockSize;
+
+            if (piece.Length != expectedChunkSize)
+            {
+                _logger.Warn(
+                    "Cannot reassemble metadata: piece {0} length {1} does not match expected {2}",
+                    i,
+                    piece.Length,
+                    expectedChunkSize);
+                return null;
+            }
+
+            Array.Copy(piece, 0, assembledRawBytes, offset, piece.Length);
+            offset += piece.Length;
+        }
+
+        if (!ValidateMetadata(assembledRawBytes, expectedInfoHash, torrentName))
+        {
+            return null;
+        }
+
+        return assembledRawBytes;
+    }
+
+    public byte[] ReassembleMetadata(IEnumerable<MetadataMessage> messages, int totalSize, string expectedInfoHash, string torrentName = null)
+    {
+        if (messages == null || totalSize <= 0 || totalSize > MaxMetadataSize)
+        {
+            return null;
+        }
+
+        var totalPieces = (int)Math.Ceiling((double)totalSize / MetadataBlockSize);
+        var pieceArray = new byte[totalPieces][];
+
+        foreach (var msg in messages)
+        {
+            if (msg != null && msg.Piece >= 0 && msg.Piece < totalPieces && msg.Data != null)
+            {
+                pieceArray[msg.Piece] = msg.Data;
+            }
+        }
+
+        return ReassembleMetadata(pieceArray, totalSize, expectedInfoHash, torrentName);
+    }
+
+    public bool TryReassembleMetadata(byte[][] pieces, int totalSize, string expectedInfoHash, out byte[] assembledMetadata, string torrentName = null)
+    {
+        assembledMetadata = ReassembleMetadata(pieces, totalSize, expectedInfoHash, torrentName);
+        return assembledMetadata != null;
     }
 }
